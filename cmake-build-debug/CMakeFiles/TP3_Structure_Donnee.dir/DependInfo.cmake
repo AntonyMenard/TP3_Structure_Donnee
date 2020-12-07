04700b585f0674d626fe04a291fcd929f331e45e
Antony Ménard <antonymenard2014@gmail.com>
2020-12-07 17:02:48 -0500

    init commit

---
+++ cmake-build-debug/CMakeFiles/TP3_Structure_Donnee.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Uni/Structure_Donnee/TP3_Structure_Donnee/main.cpp" "D:/Uni/Structure_Donnee/TP3_Structure_Donnee/cmake-build-debug/CMakeFiles/TP3_Structure_Donnee.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TP3_Structure_Donnee.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP3_Structure_Donnee.dir/main.cpp.obj"
+  "TP3_Structure_Donnee.exe"
+  "TP3_Structure_Donnee.exe.manifest"
+  "TP3_Structure_Donnee.lib"
+  "TP3_Structure_Donnee.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP3_Structure_Donnee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
